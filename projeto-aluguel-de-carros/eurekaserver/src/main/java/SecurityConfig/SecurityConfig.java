b018762f84f5a7330b538ce12a657de787cf9fe6
Pedro Bandeira <pedro.amp004@gmail.com>
2024-08-03 16:07:50 -0300

    Add EurekaSeever and SecurityConfig

---
+++ projeto-aluguel-de-carros/eurekaserver/src/main/java/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,16 @@
+package SecurityConfig;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
